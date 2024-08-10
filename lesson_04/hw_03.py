46c3d4762001bcd65ddf736836f946c38696c10e
evgeniy <areinna@i.ua>
2024-08-10 21:00:43 +0300

    Added solution for lesson 04

--- lesson_04/hw_03.py
+++ lesson_04/hw_03.py
@@ -6,3 +6,10 @@
 # [1, 2, 3, 4, 5, 6, 7, 9] == [1, 3, 7]
 # [1, 1, 2, 1] == [1, 2, 2]
 # [6, 3, 7] == [6, 7, 3]
+
+
+import random
+lst = [random.randint(1, 100) for i in range(random.randint(3, 10))]
+print("Origin list: ", lst)
+new_lst = [lst[0], lst[2], lst[-2]]
+print("Modified list: ", new_lst)
