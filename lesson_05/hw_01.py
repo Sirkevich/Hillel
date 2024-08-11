5a6d4caea0059cbecc2593b3ce309325299a571c
evgeniy <areinna@i.ua>
2024-08-11 22:19:16 +0300

    Added solution for lesson 05

--- lesson_05/hw_01.py
+++ lesson_05/hw_01.py
@@ -28,3 +28,21 @@
 # m3 => True
 # assert => False
 # assert_exception => True
+
+
+import string
+import keyword
+
+
+result = True
+name = input("Введіть рядок: ")
+if len(name) > 1 and all(char == '_' for char in name):
+    result = False
+if name[0].isdigit():
+    result = False
+if any(char.isupper() or char in string.whitespace or char in string.punctuation.replace('_', '') for char in name):
+    result = False
+if name in keyword.kwlist:
+    result = False
+
+print(result)
--- lesson_05/hw_03.py
+++ lesson_05/hw_03.py
@@ -11,3 +11,15 @@
 # 'Python Community' -> #PythonCommunity
 # 'i like python community!' -> #ILikePythonCommunity
 # 'Should, I. subscribe? Yes!' -> #ShouldISubscribeYes
+
+
+punctuation_symbols = r''' !"#$%&'()*+,-./:;<=>?@[\]_^`{|}~'''
+punctuation_symbols_list = [i for i in punctuation_symbols]
+
+user_variable = input('Input your sentence: ').title()
+hashtag = "#" + ''.join([i for i in user_variable if i not in punctuation_symbols_list])
+
+if len(hashtag) > 140:
+    hashtag = hashtag[0:140]
+
+print(f'The hashtag is created: {hashtag}')
