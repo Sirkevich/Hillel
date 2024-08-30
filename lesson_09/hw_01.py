ab44ea7e0e93a8cf51b86073939176245307fb99
evgeniy <areinna@i.ua>
2024-08-30 18:38:05 +0300

    Added solution for lesson 09

--- lesson_09/hw_01.py
+++ lesson_09/hw_01.py
@@ -15,3 +15,19 @@
 # assert (popular_words('''When I was One I had just begun When I was Two I was nearly new ''',
 #                       ['i', 'was', 'three', 'near']) ==
 #         { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1' print('OK'))
+
+
+def popular_words(text: str, words: list) -> dict:
+    text_list = text.lower().split()
+    res_dict = {}
+
+    for elem in words:
+        res_dict[elem] = text_list.count(elem)
+
+    return res_dict
+
+
+result = popular_words('''When I was One I had just begun When I was Two I was nearly new ''',
+                       ['i', 'was', 'three', 'near'])
+
+print(result)
