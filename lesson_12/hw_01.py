e11dd457d5994750dbd379d796bb426d8fe4e32f
evgeniy <areinna@i.ua>
2024-09-07 14:55:13 +0300

    Added solution for lesson 12

--- lesson_12/hw_01.py
+++ lesson_12/hw_01.py
@@ -11,8 +11,33 @@
 #
 # Додаткове завдання для тих, хто захоче ускладнити рішення - спробуйте
 # прибрати рядки, в яких немає інформації.
-#
-# import codecs
-# def delete_html_tags(html_file, result_file='cleaned.txt'):
-#       with codecs.open(html_file, 'r', 'utf-8') as file:
-#            html = file.read()
+
+
+def delete_html_tags(html_file, result_file='cleaned.txt'):
+    with open(html_file, encoding='utf-8') as file:
+        input_data = file.readlines()
+
+    output_data = []
+    for line in input_data:
+        line = line.strip('\n').strip()
+        if line:
+            new_line = ''
+            tag = False
+            for symbol in line:
+                if symbol == '<':
+                    tag = True
+                    continue
+                if symbol == '>':
+                    tag = False
+                    continue
+                if tag is False:
+                    new_line += symbol
+
+            if new_line:
+                output_data.append(new_line)
+
+    with open(result_file, 'w', encoding='utf-8') as file:
+        file.write('\n'.join(output_data))
+
+
+delete_html_tags('draft.html')
